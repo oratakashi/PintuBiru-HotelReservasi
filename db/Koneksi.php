2f0379216ee9c31d4f100ea27a4001bbaad78612
Key <david.creazy@my.gavilan.edu>
2019-09-26 20:13:30 +0700

    Update Koneksi.php

--- db/Koneksi.php
+++ db/Koneksi.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $koneksi = new PDO('mysql:host=localhost;dbname=myHotel', 'root', '');
+        $koneksi = new PDO('mysql:host=remotemysql.com:3306;dbname=spNhWmF9wq', 'spNhWmF9wq', 'udQIrZdAPp');
     }catch(PDOException $e){
         echo "<p style='color:red'>Koneksi Error..</p>";
     }
